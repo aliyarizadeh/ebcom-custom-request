6ae485e0d61adc2c50a4de945ea289ee0d4183da
kh.hashemi <kh.hashemi@ebcom.ir>
2023-06-03 13:18:31 +0330

    fix: changes applied

--- src/lib/validate.js
+++ src/lib/validate.js
@@ -1,7 +1,7 @@
 const agents = require('./agents');
 
 module.exports = (params, agent, logger) => {
-  if (![true, false].includes(params.http2)) throw new Error('Http2 input is not valid!');
+  if (params.http2 !== undefined && ![true, false].includes(params.http2)) throw new Error('Http2 input is not valid!');
   if (!(agent in agents)) throw new Error('Agent not found');
   if ((params.http2 && !['got', 'http2'].includes(agent))) throw new Error('you can use http2 only with GOT and HTTP2 agents');
   if (logger && typeof logger !== 'function') throw new Error('Invalid logger service');
